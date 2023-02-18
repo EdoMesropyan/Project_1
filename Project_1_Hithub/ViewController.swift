7afa07e256116a1cfc776d5c046c61a40f005e4b
Edo Mesropyan <edomesropyan@Sonas-MacBook-Pro.local>
2023-02-18 13:50:46 +0400

    Added new proj

---
+++ Project_1_Hithub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_1_Hithub
+//
+//  Created by Edo Mesropyan on 18.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
